5dc40255f36c026d55360c27858dfe4bcb41802f
Collin Larsen <clarsen908@gmail.com>
2023-11-18 22:28:36 -0600

    Implemented vosk - doesnt work well

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import speech_recognition as sr
 import json
+from vosk import Model, KaldiRecognizer
+import sys
+import os
 
 from voice_controller import VoiceController
 
+# Load Vosk model
+if not os.path.exists("model"):
+    print("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+    exit(1)
+model = Model("model")
 
-def new_audio(mic, r):
+
+def new_audio(mic, r, recognizer):
     ''' Reset/Create the audio stream '''
     with mic as source:
+        r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
     return audio
 
@@ -23,35 +33,32 @@ def main():
     # Initialize the voice controller
     vc = VoiceController()
 
-    # Start listening for commands
+    # Initialize Vosk recognizer
+    recognizer = KaldiRecognizer(model, 16000)
+
     print('Audio commands started. Press Ctrl+C to exit')
 
-    # Require keyboard interupt to stop
     while True:
-        # Listen for a new command
-        audio = new_audio(mic, r)
-        try:
-            text = r.recognize_google(audio)
-        except sr.UnknownValueError:
+        audio = new_audio(mic, r, recognizer)
+        if recognizer.AcceptWaveform(audio.get_wav_data()):
+            try:
+                text = recognizer.Result()
+            except sr.UnknownValueError:
+                if config["Verbose"]:
+                    print("Could not understand audio")
+                continue
+
+            # Output the text to the console if what they said was recognized
             if config["Verbose"]:
-                print("Could not understand audio")
-            continue
-        except sr.RequestError as e:
-            print(
-                "Could not request results from Google Speech Recognition service; {0}".format(e))
-            continue
+                print("You said: {}".format(text))
 
-        # Output the text to the console if what they said was recognized
-        if config["Verbose"]:
-            print("You said: {}".format(text))
-
-        # Let the voice controller know what they said
-        try:
-            # Executes the command if it is a valid command
-            vc.execute(text)
-        except KeyError:
-            # What they said was not a valid command, we dont care
-            pass
+            # Let the voice controller know what they said
+            try:
+                # Executes the command if it is a valid command
+                vc.execute(text)
+            except KeyError:
+                # What they said was not a valid command, we dont care
+                pass
 
 
 if __name__ == '__main__':
